2acdb365e1470c2036893fa47832c519138fe660
Fatih Sağlam <9195208+mfatihsaglam@users.noreply.github.com>
2019-12-25 23:20:43 +0300

    Error fix
    Mistakenly, I was used @ instead of $.
    Fixed the error now.

--- kontrol/veritabani.php
+++ kontrol/veritabani.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "mydatabasename";
 try {
-    $connection = new PDO("mysql:host=$servername;dbname=@dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     // set the PDO error mode to exception
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
